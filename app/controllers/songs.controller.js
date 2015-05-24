d2cf1213eef16e7c6952b75f4d546a9bbd3a96cc
Alberto Luna <alberto.luna.95@gmail.com>
2015-05-24 11:41:03 +0200

    Now delete also remove album/artist folder
    If there is not any song of that album/artist the correspondent folder is deleted

--- app/controllers/songs.controller.js
+++ app/controllers/songs.controller.js
@@ -1,7 +1,10 @@
 var log4js = require('log4js');
+var async = require('async');
 var mainLogger = log4js.getLogger('main');
 var errorLogger = log4js.getLogger('error');
 var songLib = require('../libs/song.lib');
+var Boom = require('boom');
+var rimraf = require('rimraf');
 // app/controllers/songs.controllers.js
 /**
  * Module dependencies
@@ -95,28 +98,108 @@ exports.delete = function(req, res) {
   var findParams = getFindParams(req);
   if(!findParams) return res.status(400).send();
 
-  Song.findOne(findParams, function(err, song) {
-    if (!song) {
-      res.status(404).send('Cancion no encontrada');
-      return;
-    }
-    song.remove(function(err, song) {
-      if (err) {
-        errorLogger.error('Error deleting song');
-        errorLogger.log(err);
-        res.status(501).send('Error al borrar la cancion');
-      } else {
-        fs.unlink(song.path, function(err) {
-          if (err && process.env.NODE_ENV != 'test') {
-            errorLogger.error('Error deleting song');
-            errorLogger.log(err);
-            res.status(501).send('Error al borrar la cancion');
-            return;
+  async.waterfall([
+      function(callback) {
+        // check if song exists
+        Song.findOne(findParams, function(err, song) {
+          if(err) {
+            return callback({
+              response: Boom.badImplementation('Server error finding song'),
+              error: err
+            });
           }
-          res.status(200).send();
+          if(!song) {
+            return callback({
+              response: Boom.notFound('Song not found')
+            });
+          }
+          callback(null, song);
         });
+      },
+    function(song, callback) {
+      // remove song from database
+      song.remove(function(err) {
+        if(err) {
+          return callback({
+            response: Boom.badImplementation('Error deleting song'),
+            error: err
+          });
+        }
+        callback(null, song);
+      });
+    }, function(song, callback) {
+      // If there is not any song with that artist remove artist folder
+      Song.find({ artist: song.artist }, function(err, songs) {
+        if(err) {
+          return callback({
+            response: Boom.badImplementation('Server error finding songs by artist'),
+            error: err
+          });
+        }
+        if(!songs.length) {
+          rimraf(musicFolder + '/' + song.artist, function(err) {
+            if(err) {
+              return callback({
+                response: Boom.badImplementation('Server deleting artist song folder'),
+                error: err
+              });
+            }
+            callback(null, null);
+          });
+        } else {
+          callback(null, song);
+        }
+      });
+    }, function(song, callback) {
+      // If there is not any song with that album remove album folder
+      if(!song) {
+        return callback(null, null);
       }
-    });
+
+      Song.find({ album: song.album}, function(err, songs) {
+        if(err) {
+          return callback({
+            response: Boom.badImplementation('Server error finding songs by album'),
+            error: err
+          });
+        }
+
+        if(!songs.length) {
+          rimraf(musicFolder + '/' + song.artist + '/' + song.album, function(err) {
+            if(err) {
+              return callback({
+                response: Boom.badImplementation('Server deleting album song folder'),
+                error: err
+              });
+            }
+            callback(null, null);
+          });
+        } else {
+          callback(null, song);
+        }
+      });
+    }, function(song, callback) {
+      if(!song) {
+        return callback();
+      }
+
+      fs.unlink(song.path, function(err) {
+        if(err) {
+          return callback({
+            response: Boom.badImplementation('Server deleting album song folder'),
+            error: err
+          });
+        }
+        callback();
+      });
+    }
+  ], function(err) {
+    if(err) {
+      errorLogger.error('Error deletting song');
+      errorLogger.log(err.error);
+      return res.status(err.response.message).send(err.response);
+    }
+    res.sendStatus(204);
   });
 };
 
